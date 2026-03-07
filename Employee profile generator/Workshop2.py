2ce5f9668508c1c0865fa4824a5a7f87a0a9e6ca
Marouane <mrmarouanehajji@gmail.com>
2026-03-07 17:03:51 +0000

    organizing code and grouping it logically to make it easier to read

--- Employee profile generator/Workshop2.py
+++ Employee profile generator/Workshop2.py
@@ -1,25 +1,44 @@
+# Personal Information
 first_name = 'John'
 last_name = 'Doe'
 full_name = first_name + ' ' + last_name
+
 address = '123 Main Street'
 address += ', Apartment 4B'
+
+
+# Employee Age Information
 employee_age = 28
 employee_info = full_name + ' is ' + str(employee_age) + ' years old'
 print(employee_info)
+
+
+# Experience Information
 years_experience = 5
 experience_years = 5
 experience_info = 'Experience: ' + str(experience_years) + ' years'
 print(experience_info)
+
+
+# Job Information
 position = 'Data Analyst'
 salary = 75000
+
 employee_card = f'Employee: {full_name} | Age: {employee_age} | Position: {position} | Salary: ${salary}'
 print(employee_card)
+
+
+# Employee Code Processing
 employee_code = 'DEV-2026-JD-001'
+
 department = employee_code[0:3]
 print(department)
+
 year_code = employee_code[4:8]
 print(year_code)
+
 initials = employee_code[9:11]
 print(initials)
+
 last_three = employee_code[-3:]
 print(last_three)
